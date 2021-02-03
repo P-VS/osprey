4f3955f4e62158d7204074127c2ced73dcbc0a78
dr. Van Schuerbeek P <60138268+P-VS@users.noreply.github.com>
2021-02-03 15:47:16 +0100

    Synchronize schorschino/osprey 02/02/2021

--- load/OspreyLoad.m
+++ load/OspreyLoad.m
@@ -99,7 +99,7 @@ switch MRSCont.vendor
 end
 
 % Perform coil combination (SENSE-based reconstruction if PRIAM flag set)
-if ~MRSCont.flags.isPRIAM
+if ~MRSCont.flags.isPRIAM && ~MRSCont.flags.isMRSI
     if sum(strcmp(MRSCont.datatype, {'DATA', 'RAW', 'P'})) == 1
         [MRSCont] = osp_combineCoils(MRSCont);
     else
@@ -109,8 +109,109 @@ if ~MRSCont.flags.isPRIAM
     end
 elseif MRSCont.flags.isPRIAM
     [MRSCont] = osp_senseRecon(MRSCont);
+elseif MRSCont.flags.isMRSI
+    [MRSCont] = osp_MRSIRecon(MRSCont);
 end
 
+%% Get scaling for the plots
+% Creates y-axis range to align the load plots between datasets
+MRSCont.plot.load.match = 0; % Scaling between datasets is turned off by default
+Range = zeros(2,MRSCont.nDatasets);
+for kk = 1 : MRSCont.nDatasets
+    temp_spec = op_freqrange(MRSCont.raw{kk}, 0.2, 4.5);
+    Range(1,kk) = max(real(temp_spec.specs),[],'all');
+    Range(2,kk) = min(real(temp_spec.specs),[],'all');   
+end
+MRSCont.plot.load.mets.max = Range(1,:);
+MRSCont.plot.load.mets.min = Range(2,:);  
+MRSCont.plot.load.mets.ContMax = max(Range,[],'all');
+MRSCont.plot.load.mets.ContMin = min(Range,[],'all');
+if MRSCont.flags.hasMM       
+    Range = zeros(2,MRSCont.nDatasets);
+    for kk = 1 : MRSCont.nDatasets
+        temp_spec = op_freqrange(MRSCont.raw{kk}, 0, 4.6);
+        Range(1,kk) = max(real(temp_spec.specs),[],'all');
+        Range(2,kk) = min(real(temp_spec.specs),[],'all');   
+    end
+    MRSCont.plot.load.mm.max = Range(1,:);
+    MRSCont.plot.load.mm.min = Range(2,:);  
+    MRSCont.plot.load.mm.ContMax = max(Range,[],'all');
+    MRSCont.plot.load.mm.ContMin = min(Range,[],'all');            
+end                                
+if MRSCont.flags.hasRef
+    Range = zeros(2,MRSCont.nDatasets);
+    for kk = 1 : MRSCont.nDatasets
+        temp_spec = op_freqrange(MRSCont.raw_ref{kk}, 0, 2*4.68);
+        Range(1,kk) = max(abs(real(temp_spec.specs)),[],'all');
+        Range(2,kk) = min(abs(real(temp_spec.specs)),[],'all');   
+    end
+    MRSCont.plot.load.ref.max = Range(1,:);
+    MRSCont.plot.load.ref.min = Range(2,:);  
+    MRSCont.plot.load.ref.ContMax = max(Range,[],'all');
+    MRSCont.plot.load.ref.ContMin = min(Range,[],'all');            
+end
+if MRSCont.flags.hasWater
+    Range = zeros(2,MRSCont.nDatasets);
+    for kk = 1 : MRSCont.nDatasets
+        temp_spec = op_freqrange(MRSCont.raw_w{kk}, 0, 2*4.68);
+        Range(1,kk) = max(abs(real(temp_spec.specs)),[],'all');
+        Range(2,kk) = min(abs(real(temp_spec.specs)),[],'all');   
+    end
+    MRSCont.plot.load.w.max = Range(1,:);
+    MRSCont.plot.load.w.min = Range(2,:);     
+    MRSCont.plot.load.w.ContMax = max(Range,[],'all');
+    MRSCont.plot.load.w.ContMin = min(Range,[],'all');            
+end
+
+%% Get scaling for the plots
+% Creates y-axis range to align the process plots between datasets
+if MRSCont.flags.isPRIAM || MRSCont.flags.isMRSI
+    MRSCont.plot.load.match = 1; % Scaling between datasets is turned off by default
+else
+    MRSCont.plot.load.match = 0; % Scaling between datasets is turned off by default
+end
+
+ppmmax = 4.5;
+ppmmin = 0.2;   
+Range = zeros(2,MRSCont.nDatasets);
+for kk = 1 : MRSCont.nDatasets
+    temp_spec = op_freqrange(MRSCont.raw{kk}, ppmmin, ppmmax);
+    Range(1,kk) = max(abs(real(temp_spec.specs)),[],'all');
+    Range(2,kk) = min(abs(real(temp_spec.specs)),[],'all');            
+end
+MRSCont.plot.load.mets.max = Range(1,:);
+MRSCont.plot.load.mets.min = Range(2,:);
+MRSCont.plot.load.mets.ContMax = max(Range(1,:));
+MRSCont.plot.load.mets.ContMin = min(Range(2,:)); 
+
+if MRSCont.flags.hasRef
+    ppmmax = 2*4.68;
+    ppmmin = 0;   
+    Range = zeros(2,MRSCont.nDatasets);
+    for kk = 1 : MRSCont.nDatasets
+        temp_spec = op_freqrange(MRSCont.raw_ref{kk}, ppmmin, ppmmax);
+        Range(1,kk) = max(abs(real(temp_spec.specs)),[],'all');
+        Range(2,kk) = min(abs(real(temp_spec.specs)),[],'all');            
+    end
+    MRSCont.plot.load.ref.max = Range(1,:);
+    MRSCont.plot.load.ref.min = Range(2,:);
+    MRSCont.plot.load.ref.ContMax = max(Range(1,:));
+    MRSCont.plot.load.ref.ContMin = min(Range(2,:)); 
+end
+if MRSCont.flags.hasWater
+    ppmmax = 2*4.68;
+    ppmmin = 0;   
+    Range = zeros(2,MRSCont.nDatasets);
+    for kk = 1 : MRSCont.nDatasets
+        temp_spec = op_freqrange(MRSCont.raw_w{kk}, ppmmin, ppmmax);
+        Range(1,kk) = max(abs(real(temp_spec.specs)),[],'all');
+        Range(2,kk) = min(abs(real(temp_spec.specs)),[],'all');            
+    end
+    MRSCont.plot.load.w.max = Range(1,:);
+    MRSCont.plot.load.w.min = Range(2,:);
+    MRSCont.plot.load.w.ContMax = max(Range(1,:));
+    MRSCont.plot.load.w.ContMin = min(Range(2,:)); 
+end
 %% Clean up and save
 % Set exit flags and version
 MRSCont.flags.didLoadData           = 1;
