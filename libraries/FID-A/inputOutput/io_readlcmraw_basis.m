85e5b1622a4d9dd9180ed3817d144e1f579747f7
dr. Van Schuerbeek P <60138268+P-VS@users.noreply.github.com>
2021-02-03 16:04:38 +0100

    Synchronize schorschino/osprey 02/02/2021

--- libraries/FID-A/inputOutput/io_readlcmraw_basis.m
+++ libraries/FID-A/inputOutput/io_readlcmraw_basis.m
@@ -132,8 +132,9 @@ while ~feof(fid)
     ppm=ppm+4.68;
     t=[dwelltime:dwelltime:vectorsize*dwelltime];
     txfrq=hzpppm*1e6;
-    mName=split(metabName,'-');
-    metabName = mName{numel(mName)};
+    if strcmp(metabName,'-CrCH2')
+        metabName = 'CrCH2';
+    end
     eval(['out.' metabName '.fids=fids;']);
     eval(['out.' metabName '.specs=specs;']);
     eval(['out.' metabName '.sz=[vectorsize 1 1 1];']);
